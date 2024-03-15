ca074e47da9c5b2cc85bf765347a5adc1d3ebf37
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-03-15 16:52:19 +0600

    added user info and token in cookies

--- src/redux/features/store.ts
+++ src/redux/features/store.ts
@@ -1,7 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./auth/authSlice";
+import { baseApi } from "../api/baseApi";
+// import authReducer from "../features/auth/authSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    // auth: authReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares().concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
