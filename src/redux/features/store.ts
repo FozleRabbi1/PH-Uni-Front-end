5d8c82438e25993d96e28b2cb623ee98ae32f2f3
Fozle Rabbi <fozlerabbi9790@gmail.com>
2024-03-18 13:08:24 +0600

    set redux persist and create protected route

--- src/redux/features/store.ts
+++ src/redux/features/store.ts
@@ -1,17 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
 import { baseApi } from "../api/baseApi";
-// import authReducer from "../features/auth/authSlice";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const presistAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
-    // auth: authReducer,
     [baseApi.reducerPath]: baseApi.reducer,
-    auth: authReducer,
+    auth: presistAuthReducer,
   },
   middleware: (getDefaultMiddlewares) =>
-    getDefaultMiddlewares().concat(baseApi.middleware),
+    getDefaultMiddlewares({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
